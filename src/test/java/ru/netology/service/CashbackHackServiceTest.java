0374f965ad265f71d9bcbb4d8bb9b2e675e86e5d
Casa Bonita <pol33@rambler.ru>
2020-02-09 09:37:46 +0300

    Change CashbackHackServiceTest.java

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -7,6 +7,52 @@ import static org.junit.jupiter.api.Assertions.*;
 class CashbackHackServiceTest {
 
     @Test
-    void remain() {
+        // проверка, что тест "упадёт"
+    void shouldReturn200IfAmountIs900() {
+
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 900;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 200;
+        assertEquals(expected, actual);
     }
+
+    @Test
+        // проверка, что тест работает, условие выполняется
+    void shouldReturn100IfAmountIs900() {
+
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 900;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 100;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+        // проверка пограничного значения minimum
+    void shouldReturn999IfAmountIs1() {
+
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 999;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+        // проверка пограничного значения maximum (оно же minimum для следующего диапазона)
+    void shouldReturn0IfAmountIs1000() {
+
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+
 }
